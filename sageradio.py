b32accd80bece83c19a4204605aa8344e0b8c822
Sage Hahn <shahn@sram.com>
2024-08-17 22:17:43 -0500

    working display, added system volume max

--- sageradio.py
+++ sageradio.py
@@ -1,5 +1,3 @@
-
-
 import os
 import signal
 import subprocess
@@ -8,21 +6,22 @@ import random
 import RPi.GPIO as GPIO
 import time
 
+from display import Display
 
 GPIO.setmode(GPIO.BCM)
 
 #google configuring pulse audio for mono - it works! 
 
 streams_loc = "/home/pi/sageradio/"
-streams = ["wfmu.pls", "wfmu_rock.pls", "DI.FM - Trance.pls", "chirpradio.m3u", "ICRTFM-MP3.m3u", "bayern1obb_2.m3u"]
+streams = ["WFMU.pls", "ROCK.pls", "DIFM-Trance.pls", "CHRP.m3u", "ICRT.m3u", "BAY1.m3u"]
 
 def play(cmd):
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True, preexec_fn = os.setsid)
     return p
 
-switches = [5,6,4,17,27,22]
-station_led = 10
-power_sw = 9
+switches = [17, 27, 22, 5, 6, 26, 23, 24, 25, 8, 7]
+station_led = 12
+power_sw = 16
 
 GPIO.setup([power_sw], GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
@@ -43,17 +42,20 @@ for i in range(5):
 
 os.system("killall vlc")
 
+# Max out the volume
+os.system("amixer set Master 65535")
 def get_active_index():
     active_index = -1
     for index, switch in enumerate(switches):
         if not(GPIO.input(switch)):
             active_index = index
             break
-
     return active_index
 
 active_index = -1
-last_active_index = -1                
+last_active_index = -1
+
+d = Display()
 while True:
     
     active_index = get_active_index()
@@ -63,7 +65,9 @@ while True:
         GPIO.output(station_led, GPIO.LOW)
         os.system("killall vlc")
         last_active_index = -1
+        d.clear()
     else:
+        #display "static" on LED
         if active_index == -1:
             val = random.random()
             if val < 0.2:
@@ -94,9 +98,14 @@ while True:
                     pass
                 GPIO.output(station_led, GPIO.HIGH)
                 
-                cmd = "cvlc {}\"{}\"".format(streams_loc, streams[active_index%len(streams)])
+                stream = streams[active_index%len(streams)]
+                stream_name = stream[0:4] 
+                #stream_name = stream.split('-')[0]
+                cmd = "cvlc {}\"{}\"".format(streams_loc, stream)
                 print(cmd)
 
                 p = play(cmd)
+                print(stream_name)    
+                d.set_channel(stream_name)
 
     time.sleep(0.05)
