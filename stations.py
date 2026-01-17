3818c060f20b1dcf55427d3c0c3956cb83ab88dc
Sage Hahn <shahn@sram.com>
2026-01-17 14:00:28 -0600

    Added wdrt

--- stations.py
+++ stations.py
@@ -4,13 +4,14 @@ stations = {
         "WFMU": "http://stream0.wfmu.org/freeform-high.aac",
         "FM4 ": "https://orf-live.ors-shoutcast.at/fm4-q2a",
         "ROCK": "http://wfmu.org/wfmu_rock.pls",
-        "KEXP": "http://live-aacplus-64.kexp.org/kexp64.aac",
+        "WDRT": "https://audio.wdrt.org/high?type=.mp3",
         "CHRP": "http://chirpradio.org/stream",
         "ICRT": "https://radio.garden/api/ara/content/listen/33msy__D/channel.mp3",
+        "KEXP": "http://live-aacplus-64.kexp.org/kexp64.aac",
         "DIFM": "http://prem1.di.fm:80/trance?09f92106bd5141ab",
         "D&B!": "http://prem1.di.fm:80/liquiddnb?09f92106bd5141ab",
-        "WBEZ": "http://stream.wbez.org/wbez128.mp3",
         "BBC World service":"http://wsdownload.bbc.co.uk/worldservice/meta/live/shoutcast/mp3/eieuk.pls",
+        "WBEZ": "http://stream.wbez.org/wbez128.mp3",
         }
 
         
